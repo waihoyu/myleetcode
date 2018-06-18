49e3654c84509185d7130a614943a29ec9192a44
WaiHoYu <WaiHoYu@lenovo-abc>
2018-06-18 18:48:21 +0800

    算法代码更新

--- src/myCode/LongestPalindromicSubstring_5.java
+++ src/myCode/LongestPalindromicSubstring_5.java
@@ -16,8 +16,7 @@ public class LongestPalindromicSubstring_5 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Solution_5 execute = new Solution_5();
-		System.out.println(execute.longestPalindrome("nmngaowrbsssvihklwmuqshcddwlxrywrlwtennwfvrevgvhsvgeccfulmuvrcksdmgeqrblnlwoepefhcwhmgyvgcoyyygrmttyfycxwbqktpur"));
-		;
+		System.out.println(execute.longestPalindrome("abacbcabababababababababababababababababababababababababababababababababababababababababababababababababababababab"));
 	}
 }
 
@@ -28,49 +27,56 @@ class Solution_5 {
 		String resultString = "";
 		boolean isPossbile = true;
 		char[] longestPal = inputs.toCharArray();
-		int start = 0;
-		String lastCheck = "";
-		String currentCheck = "";
-		String thisMax = "";
-		for (int i = 0; i < longestPal.length; i++) {
-			start = i;	
-			lastCheck ="";
-			for (int j = start; j < longestPal.length; j++) {
-				currentCheck ="";
-				currentCheck = lastCheck + longestPal[j];	
-				lastCheck = currentCheck;
-				char[] checkPal = currentCheck.toCharArray();
-				int a = 0;
-				int b = checkPal.length - 1;				
-				while(true) {
-					if ((a != b) && (a <= b)) {
-						if ((checkPal[a] == checkPal[b])) {							
-							a++;
-							b--;
-							isPossbile  = true;
+		if (longestPal.length <= 1000) {
+			int start = 0;
+			String lastCheck = "";
+			String currentCheck = "";
+			String thisMax = "";
+			for (int i = 0; i < longestPal.length; i = i+1) {
+				start = i;	
+				lastCheck ="";
+				for (int j = start; j < longestPal.length; j++) {
+					currentCheck = "";
+					currentCheck = lastCheck + longestPal[j];	
+					lastCheck = currentCheck;
+					char[] checkPal = currentCheck.toCharArray();
+					int a = 0;
+					int b = checkPal.length - 1;				
+					while(true) {
+						if ((a != b) && (a <= b)) {
+							if ((checkPal[a] == checkPal[b])) {							
+								a++;
+								b--;
+								isPossbile  = true;
+							}
+							else {
+								isPossbile  = false;						
+								break;
+							}	
 						}
-						else {
-							isPossbile  = false;						
+						else
+						{
+							isPossbile  = true;
 							break;
-						}	
+						}
 					}
-					else
-					{
-						isPossbile  = true;
-						break;
+					if (isPossbile) {
+						thisMax  = currentCheck;	
 					}
-
+					isPossbile =false;
 				}
-				if (isPossbile) {
-					thisMax  = currentCheck;	
-				}
-				isPossbile =false;
-			}	
-			if (resultString.length() < thisMax.length()) {
-				resultString = thisMax;
-			}			
-		}		
-		return resultString;
+				char[] arrr0 = resultString.toCharArray();
+				char[] arrr1 = thisMax.toCharArray();
+				if (arrr0.length < arrr1.length) {
+					resultString = thisMax;
+				}			
+			}		
+			return resultString;			
+		}
+		else
+		{
+			return "";
+		}
 	}
 	
 }
