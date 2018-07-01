6badf81ca93b366469530df646cd626d31fa7903
WaiHoYu <WaiHoYu@lenovo-abc.mshome.net>
2018-07-01 23:42:35 +0800

    reverse integer

---
+++ src/myCode/ReverseInteger.java
@@ -0,0 +1,42 @@
+package myCode;
+
+import java.util.ArrayList;
+
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
+public class ReverseInteger {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Solution_ReverseInteger instance = new Solution_ReverseInteger();
+		System.out.println(instance.reverse(234567899));;
+
+	}
+
+}
+
+
+
+class Solution_ReverseInteger {
+    public int reverse(int x) {
+    	ArrayList<Integer> middleList = new ArrayList<Integer>();
+    	long result = 0;
+    	int result2 = 0;
+    	int a = x;
+    	int listSize = 0;
+    	while(a != 0 ) {
+    		middleList.add(a % 10);
+    		a = a/10;   		
+    	}
+    	listSize = middleList.size() - 1;
+		for (int i = 0; i < middleList.size(); i++) {
+			result = (long) (result + middleList.get(i) * Math.pow(10, listSize--));
+		}	
+		if ((result > Math.pow(2, 31) - 1) || (result < Math.pow(-2, 31))) {
+			result2 = 0;
+		}else {
+			result2 = (int)result;
+		}
+    	return result2;      
+    }
+}
---
+++ src/myCode/Sum3.java
@@ -0,0 +1,55 @@
+package myCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sum3 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] nums = { -1, 0, 1, 2, -1, -4 };
+		Solution_Sum3 instance = new Solution_Sum3();
+		instance.threeSum(nums);
+	}
+}
+
+class Solution_Sum3 {
+	@SuppressWarnings("unchecked")
+	public List<List<Integer>> threeSum(int[] nums) {
+
+		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+		ArrayList<Integer> conversionList = new ArrayList<Integer>();
+
+		ArrayList<Integer> tempList = null;
+		ArrayList<Integer> tempList2 = null;
+		@SuppressWarnings("unused")
+		ArrayList<Integer> tempList3 = null;
+		for (int i = nums.length - 1; i > -1; i--) {
+			conversionList.add(nums[i]);
+		}
+		tempList = (ArrayList<Integer>) conversionList.clone();
+		for (int i = conversionList.size() - 1; i > -1; i--) {
+			int nums_1 = tempList.get(i);
+			tempList.remove(i);
+			tempList2 = (ArrayList<Integer>) tempList.clone();
+			;
+			for (int j = tempList2.size() - 1; j > -1; j--) {
+				int nums_2 = tempList2.get(j);
+				tempList2.remove(j);
+				tempList3 = (ArrayList<Integer>) tempList2.clone();
+				for (int k = tempList3.size() - 1; k > -1; k--) {
+					int nums_3 = tempList3.get(k);
+					if (nums_1 + nums_2 + nums_3 == 0) {
+						ArrayList<Integer> list2 = new ArrayList<Integer>();
+						list2.add(nums_1);
+						list2.add(nums_2);
+						list2.add(nums_3);
+						result.add(list2);
+						
+					}
+				}
+			}
+		}
+		System.out.println(result);	
+		return null;
+	}
+}
