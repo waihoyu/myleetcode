62f1b051d1c4984aa7fa6a330980657b538ed9f0
waihoyu <13701343809@qq.com>
2018-07-27 19:22:22 +0800

    nothing

--- src/_0016_Array/MyArray.java
+++ src/_0016_Array/MyArray.java
@@ -1,10 +1,14 @@
 package _0016_Array;
 
+
+
 public final class MyArray {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+while(true) {
+	System.out.println(new java.util.Date().toString() + "疫苗事件" + new java.util.Date().toString() + "国务院会议");
+}
 	}
 
 }
